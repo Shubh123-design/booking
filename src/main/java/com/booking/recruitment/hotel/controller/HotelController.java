b3e66dcb96ce7a3063c3281637555a41255bf415
Shubh123-design <shubhangipriya191@gmail.com>
2025-07-03 17:46:30 +0530

    Update HotelController.java

--- src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
+++ src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
@@ -24,42 +24,6 @@ public class HotelController {
     return hotelService.getAllHotels();
   }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Hotel createHotel(@RequestBody Hotel hotel) {
-    return hotelService.createNewHotel(hotel);
-  }
-
-
-  package com.booking.recruitment.hotel.controller;
-
-import com.booking.recruitment.hotel.exception.ElementNotFoundException;
-import com.booking.recruitment.hotel.model.Hotel;
-import com.booking.recruitment.hotel.service.HotelService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/hotel")
-public class HotelController {
-  private final HotelService hotelService;
-
-  @Autowired
-  public HotelController(HotelService hotelService) {
-    this.hotelService = hotelService;
-  }
-
-  @GetMapping
-  @ResponseStatus(HttpStatus.OK)
-  public List<Hotel> getAllHotels() {
-    return hotelService.getAllHotels();
-  }
-
   @GetMapping(path = "/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Hotel getHotelById(@PathVariable Long id) {
