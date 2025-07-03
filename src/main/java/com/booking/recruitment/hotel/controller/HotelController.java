f089eba3b7220ee208058750e5817fb9c2a74b29
Shubh123-design <shubhangipriya191@gmail.com>
2025-07-03 17:45:22 +0530

    Update HotelController.java

--- src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
+++ src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
@@ -29,4 +29,54 @@ public class HotelController {
   public Hotel createHotel(@RequestBody Hotel hotel) {
     return hotelService.createNewHotel(hotel);
   }
+
+
+  package com.booking.recruitment.hotel.controller;
+
+import com.booking.recruitment.hotel.exception.ElementNotFoundException;
+import com.booking.recruitment.hotel.model.Hotel;
+import com.booking.recruitment.hotel.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+  private final HotelService hotelService;
+
+  @Autowired
+  public HotelController(HotelService hotelService) {
+    this.hotelService = hotelService;
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public List<Hotel> getAllHotels() {
+    return hotelService.getAllHotels();
+  }
+
+  @GetMapping(path = "/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Hotel getHotelById(@PathVariable Long id) {
+    Optional<Hotel> hotelOptional = hotelService.getHotelById(id);
+    return hotelOptional.orElseThrow(() -> new ElementNotFoundException("Hotel not found"));
+  }
+
+  @DeleteMapping(path = "/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Hotel deleteHotelById(@PathVariable Long id) {
+    return hotelService.deleteHotelById(id);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Hotel createHotel(@RequestBody Hotel hotel) {
+    return hotelService.createNewHotel(hotel);
+  }
+
 }
